730d8445c6789cf64c318b0c6a6125173ec103ad
devshankha <45039485+devshankha@users.noreply.github.com>
2020-11-21 15:46:03 +0530

    Update FindNonRepeated.java

--- FindNonRepeated.java
+++ FindNonRepeated.java
@@ -3,6 +3,8 @@ public class FindNonRepeated {
 	public static void main(String[] args) {
 		int arr[] = {1,1,2,2,3,4,4};
 		int val = 0;
+		//check this out, the iterations in the loop is very important,
+		//it should be arr.length and NOT arr.length-1
 		for (int i=0 ; i < arr.length; i++) {
 			val = val^arr[i];
 			
